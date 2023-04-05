af53dbf669c744ba4bb9a0cade93f79b33a694f9
Kaan <kenkaraa@gmail.com>
2023-04-05 14:28:29 -0400

    Spelling mistake and debug

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/container/Home/Home";
+import About from './components/container/About/About.jsx'
+
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
     </div>
   );
 };
---
+++ src/components/container/About/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+
+  return(
+  <div className="container" id="about">
+    <div className="title">
+      <span>Who Am I?</span>
+      <h1>About Me</h1>
+    </div>
+  </div>
+)};
+export default About;
