833032672e0485159ed2ed5420ee4377e6d3d0de
Kaan <kenkaraa@gmail.com>
2023-04-05 14:16:14 -0400

    Adds About component and Font

---
+++ src/components/container/Home/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./About.scss";
+
+const About = () => {
+    <div className="container" id='about'>
+        <div className="tittle">
+            <span>Who Am I?</span>
+            <h1>About Me</h1>
+        </div>
+    </div>
+}
