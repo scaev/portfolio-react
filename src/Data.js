3a8a34236aaccb331a48bbfb86e5c082649a9d29
Kaan <kenkaraa@gmail.com>
2023-04-06 14:21:29 -0400

    Adds icons to skills component

--- src/Data.js
+++ src/Data.js
@@ -1,8 +1,33 @@
-import { FaHtml5, FaCss3, FaNodeJs, FaReact, FaSass, FaFigma, FaPhoneAlt, FaUser, FaMapMarkerAlt, FaPaperPlane, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3,
+  FaNodeJs,
+  FaReact,
+  FaSass,
+  FaFigma,
+  FaPhoneAlt,
+  FaUser,
+  FaMapMarkerAlt,
+  FaPaperPlane,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+} from "react-icons/fa";
+// import {
+//   faCss3,
+//   faGithub,
+//   faHtml5,
+//   faJs,
+//   faMdb,
+//   faNode,
+//   faPython,
+//   faReact,
+//   faChrome,
+// } from "@fortawesome/free-brands-svg-icons";
 import { DiJavascript } from "react-icons/di";
 import { SiTypescript } from "react-icons/si";
 import { BsTwitter } from "react-icons/bs";
-import portfolio from "./assets/portfolio.jpg"
+import portfolio from "./assets/portfolio.jpg";
 import work1 from "./assets/work1.png";
 import work2 from "./assets/work2.png";
 import work3 from "./assets/work3.png";
@@ -10,97 +35,115 @@ import work4 from "./assets/work4.png";
 import work5 from "./assets/work5.png";
 import work6 from "./assets/work6.png";
 
-export const navLinks = ["home", "about", "skills", "portfolio", "contact"]
+export const navLinks = ["home", "about", "skills", "portfolio", "contact"];
 
 export const socialIcons = [
   <FaGithub />,
   <FaInstagram />,
   <FaLinkedin />,
-  <BsTwitter />
-]
+  <BsTwitter />,
+];
 
 export const bios = [
   {
     id: 1,
     icon: <FaUser />,
     key: "Name",
-    value: "Kaan Karadag"
+    value: "Kaan Karadag",
   },
   {
     id: 2,
     icon: <FaPhoneAlt />,
     key: "Phone",
-    value: "+16475546296"
+    value: "+16475546296",
   },
   {
     id: 3,
     icon: <FaPaperPlane />,
     key: "Email",
-    value: "kenkaraa@gmail.com"
-  }
-]
+    value: "kenkaraa@gmail.com",
+  },
+];
 
-export const icons = [<FaHtml5 />, <FaCss3 />, <DiJavascript />, <SiTypescript />, <FaReact />, <FaNodeJs />, <FaSass />, <FaFigma />]
-
+export const icons = [
+  <FaHtml5 />,
+  <FaCss3 />,
+  <DiJavascript />,
+  <SiTypescript />,
+  <FaReact />,
+  <FaNodeJs />,
+  <FaSass />,
+  <FaFigma />,
+];
 
 export const workImages = [
   {
     id: 1,
     img: work1,
     name: "Slot Game",
-    category: "web"
+    category: "web",
   },
   {
     id: 2,
     img: work2,
     name: "Event Library",
-    category: "app"
+    category: "app",
   },
   {
     id: 3,
     img: work3,
     name: "eAuctions",
-    category: "app"
+    category: "app",
   },
   {
     id: 4,
     img: work4,
     name: "Time Capsule",
-    category: "app"
+    category: "app",
   },
   {
     id: 5,
     img: work5,
     name: "Code Mate",
-    category: 'app'
+    category: "app",
   },
   {
     id: 6,
     img: work6,
     name: "Old Portfolio",
-    category: "design"
-  }
-]
+    category: "design",
+  },
+];
 
-
-export const workNavs = [
-  "All", "Web", "App", "Design"
-]
+export const workNavs = ["All", "Web", "App", "Design"];
 
 export const contacts = [
   {
     id: 1,
     icon: <FaMapMarkerAlt />,
-    infoText: "71 Godstone Road, North York , Toronto"
+    infoText: "71 Godstone Road, North York , Toronto",
   },
   {
     id: 2,
     icon: <FaPaperPlane />,
-    infoText: "kenkaraa@gmail.com"
+    infoText: "kenkaraa@gmail.com",
   },
   {
     id: 3,
     icon: <FaPhoneAlt />,
-    infoText: "+1-647-554-6296"
-  }
-]
+    infoText: "+1-647-554-6296",
+  },
+];
+
+// export const icons = [
+//   <faCss3 />,
+//   <faGithub />,
+//   <faHtml5 />,
+//   <faJs />,
+//   <faMdb />,
+//   <faNode />,
+//   <faPython />,
+//   <FaFigma />,
+//   <faReact />,
+//   <FaSass />,
+// ];
--- src/components/container/Skills/Skills.jsx
+++ src/components/container/Skills/Skills.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skills.scss";
+import { icons } from "../../../Data";
 
 const Skills = () => {
+  const [active, setActive] = useState(1);
+
   return (
     <div className="container" id="skills">
       <div className="title">
@@ -9,8 +12,28 @@ const Skills = () => {
         <h1>Skills And Experience</h1>
       </div>
       <div className="select">
-        <button>Skills</button>
-        <button>Experience</button>
+        <button
+          onClick={() => setActive(1)}
+          className={active === 1 ? "active" : ""}
+        >
+          Skills
+        </button>
+        <button
+          onClick={() => setActive(2)}
+          className={active === 2 ? "active" : ""}
+        >
+          Experiences
+        </button>
+      </div>
+      <div className="skills">
+        {active === 1 &&
+          icons.map((icon, index) => {
+            return (
+              <div key={index} className="tools">
+                {icon}
+              </div>
+            );
+          })}
       </div>
     </div>
   );
